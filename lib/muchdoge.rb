728fc2bebce4e60684e4cd5561799aada47b38fe
addisonbean <addisonbean@gmail.com>
2014-06-15 23:57:47 -0500

    Better support for colors and arrays

--- lib/muchdoge.rb
+++ lib/muchdoge.rb
@@ -1,16 +1,23 @@
 require "muchdoge/version"
 require "dogeify/all"
 
-module Kernal
-	def puts(*args)
-		$stdout.puts(*args.map { |arg|
-			arg.dogeify rescue arg
-		})
+def puts(*args)
+	regex = /\e\[(\d+)(;\d+)*m/
+	args.each do |arg|
+		case arg
+		when String
+			$stdout.puts(arg.gsub(regex, '').dogeify)
+		when Array
+			arg.map{|e|e.gsub(regex, "") if e.is_a? String}.dogeify.each do |phrase|
+				puts(phrase)
+			end
+		else 
+			# this isn't working
+
+			puts(arg.to_s)
+			# $stdout.puts(arg)
+		end
 	end
+	return nil
 end
 
-def puts(*args)
-	$stdout.puts(args.map { |arg|
-		arg.dogeify rescue arg
-	})
-end
